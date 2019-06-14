9bcc18088356b60db1479a8f9c5aa6a4014ed192
Arun R kumar <smartboyarun56@gmail.com>
2019-06-14 12:01:00 +0530

    Checking Operation added . Message window finished

--- password_cracker.py
+++ password_cracker.py
@@ -4,6 +4,7 @@ import time
 
 # Brute Force Function
 pswrd_files = [f for f in listdir('password DB//') if f.endswith('.txt')]
+print(pswrd_files)
 i = 0
 attempt = 0
 
@@ -12,29 +13,29 @@ def brute(password_set):
     global attempt
     global i
     start = time.time()
-    f = open('password DB//'+pswrd_files[i],'r')
-    txts = f.read()
-    f_list = txts.split('\n')
-    for letter in f_list:
-        attempt += 1
-        if letter == password_set:
-            end = time.time()
-            time_taken = end - start
-            f.close()
-            return attempt, time_taken
+    while i < len(pswrd_files):
+        f = open('password DB//'+pswrd_files[i],'r')
+        txts = f.read()
+        f_list = txts.split('\n')
+        i += 1
+        for letter in f_list:
+            attempt += 1
+            if letter == password_set:
+                end = time.time()
+                time_taken = end - start
+                f.close()
+                return True
+    else:
+        return False
 
 
 def cracker(pswd):
     global i
     while True:
         try:
-            tries, timetaken = brute(pswd)
-            print('Yeah boi')
-            return True, tries, timetaken
-            break
+            flag = brute(pswd)
+            return flag
         except (TypeError, UnicodeDecodeError):
             i += 1
-        except IndexError:
-            return False, 0, 0
 
 
--- paswrd_cracker_gui.py
+++ paswrd_cracker_gui.py
@@ -6,62 +6,57 @@ import imageio
 import threading
 
 
-video_name = "watchdogs_loading.gif" #This is your video file path
+video_name = "watchdogs_loading.gif"
 video = imageio.get_reader(video_name)
 
 
-def stream(label):
-    global end
+def stream(label, hack_flag):
     for image in video.iter_data():
-        time.sleep(0.05)
         frame_image = ImageTk.PhotoImage(Image.fromarray(image))
         label.config(image=frame_image)
         label.image = frame_image
+        time.sleep(0.05)
     else:
-        end = True
+        if hack_flag:
+            hacked_pic = Image.open('hacked.jpg')
+            hacked_pic.thumbnail((300, 400))
+            tk_hacked_pic = ImageTk.PhotoImage(hacked_pic)
+            hacked_label = Label(window, image=tk_hacked_pic, bg='black')
+            hacked_label.image = tk_hacked_pic
+            hacked_label.pack()
+            text_message = ' Your Password has been identified as leaked or weak \n' \
+                           ' Please change your password in respective website'
+            message_label = Label(window, text=text_message, font=('HACKED', 14, 'bold'), bg='black', fg='white')
+            message_label.pack()
+        else:
+            congrats_message = 'Your password is not found in the database. \n' \
+                                'But we recommend you to change the \n' \
+                               'password frequently to stay safe. \n' \
+                                'Happy Internet :) </>'
+            message_label = Label(window, text=congrats_message, font=('HACKED', 14, 'bold'), bg='black', fg='white')
+            message_label.pack()
 
 
 def check_button_press():
     global end
+    global window
     end = False
     password = password_entry.get()
-    hacked, attempts, time_taken = pc.cracker(password)
     window = Toplevel(root)
     window.configure(bg='black')
-    window.geometry('600x300')
+    window.geometry('600x350')
     gif_label = Label(window, bg='black')
     gif_label.place(x=0, y=0, relwidth=1, relheight=1)
-    thread = threading.Thread(target=stream, args=(gif_label,))
+    hacked = pc.cracker(password)
+    thread = threading.Thread(target=stream, args=(gif_label, hacked,))
     thread.daemon = 1
     thread.start()
-    if hacked:
-        while not end:
-            continue
-        else:
-            hacked_pic = Image.open('hacked.jpg')
-            tk_hacked_pic = ImageTk.PhotoImage(hacked_pic)
-            hacked_label = Label(window, image=tk_hacked_pic)
-            hacked_label.pack()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 if __name__ == '__main__':
     # Main window Background setup
     root = Tk()
-    size = (600, 400)
+    size = (600, 450)
     root.geometry('600x300')
     bg_img = Image.open('DEDSEC_2560x1440.jpg')
     bg_img.thumbnail(size)
@@ -88,9 +83,6 @@ if __name__ == '__main__':
     text_prompt.grid(row=1, column=2)
     submit_button = Button(frame, text='Check', font=('Monospace', 12), bg='Grey', fg='Black',command=check_button_press)
     submit_button.grid(row=3, column=2)
-
-
-
 
     root.title('Password Checker')
     root.mainloop()
